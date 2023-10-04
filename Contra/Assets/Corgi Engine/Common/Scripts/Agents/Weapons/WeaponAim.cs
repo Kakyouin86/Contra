4a57182d72e8cfd05b0102bea5d54bfa23b575f2
Kakyouin86 <72977925+Kakyouin86@users.noreply.github.com>
2023-10-03 21:47:12 -0300

    Fix in positions

--- Contra/Assets/Corgi Engine/Common/Scripts/Agents/Weapons/WeaponAim.cs
+++ Contra/Assets/Corgi Engine/Common/Scripts/Agents/Weapons/WeaponAim.cs
@@ -445,9 +445,9 @@ namespace MoreMountains.CorgiEngine
 			}
 			// otherwise we lerp the rotation
 			else
-			{				
-				transform.rotation = Quaternion.Lerp (transform.rotation, newRotation, WeaponRotationSpeed * Time.deltaTime);
-			}
+			{
+                transform.rotation = Quaternion.Lerp (transform.rotation, newRotation, WeaponRotationSpeed * Time.fixedDeltaTime); ////Leo Monge: Need to ALWAYS bring it after update. It was Time.deltaTime
+            }
 
 			if (InstantFlip && (WasFacingRightLastFrame != _weapon.Owner.IsFacingRight))
 			{
