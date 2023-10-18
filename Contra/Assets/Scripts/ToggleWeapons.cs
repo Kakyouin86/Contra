9b55aa09920a0a292b27d014e67aa4e2ac3dbab5
Kakyouin86 <72977925+Kakyouin86@users.noreply.github.com>
2023-10-18 19:41:03 -0300

    More...

---
+++ Contra/Assets/Scripts/ToggleWeapons.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class ToggleWeapons : MonoBehaviour
+{
+    public string torsoTag = "Torso";
+    public string machineGunLightsTag = "MachineGunLights";
+    public GameObject torsoObject;
+    public GameObject machineGunObject;
+    public bool torsoObjectActive = true;
+    public bool machineGunActive = false;
+
+    void Start()
+    {
+        torsoObject = GameObject.FindGameObjectWithTag(torsoTag);
+        machineGunObject = GameObject.FindGameObjectWithTag(machineGunLightsTag);
+
+        // Ensure both objects are initially in the correct state
+        torsoObject.SetActive(torsoObjectActive);
+        machineGunObject.SetActive(!torsoObjectActive);
+        machineGunActive = !torsoObjectActive;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            if (torsoObjectActive)
+            {
+                torsoObjectActive = false;
+                machineGunActive = true;
+
+                // Set object states accordingly
+                torsoObject.SetActive(false);
+                machineGunObject.SetActive(true);
+            }
+            else
+            {
+                machineGunActive = false;
+                torsoObjectActive = true;
+
+                // Set object states accordingly
+                machineGunObject.SetActive(false);
+                torsoObject.SetActive(true);
+            }
+        }
+    }
+}
