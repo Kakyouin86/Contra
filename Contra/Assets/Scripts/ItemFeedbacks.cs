3e758e1076efbb435ab671f8b0181c6065f82cca
Kakyouin86 <72977925+Kakyouin86@users.noreply.github.com>
2024-02-15 09:40:08 -0300

    Fixes

--- Contra/Assets/Scripts/ItemFeedbacks.cs
+++ Contra/Assets/Scripts/ItemFeedbacks.cs
@@ -4,21 +4,37 @@ using UnityEngine;
 public class ItemFeedbacks : MonoBehaviour
 {
     public GameObject thePickup;
+    public GameObject theSmokeEffect;
     public Health health;
     public bool hasInstantiated = false;
+    public Vector3 offset1 = new Vector3(0.35f, 1.5f, 0f);
+    public Vector3 offset2 = new Vector3(-0.35f, 1.5f, 0f);
+    public float jumpForce = 5f;
 
     void Start()
     {
         health = GetComponent<Health>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (health.CurrentHealth <= 0 && !hasInstantiated)
         {
             GetComponent<Animator>().SetBool("Broken", true);
-            Instantiate(thePickup, transform.position, transform.rotation);
+            if (theSmokeEffect != null)
+            {
+                Instantiate(theSmokeEffect, transform.position + offset1, transform.rotation);
+                Instantiate(theSmokeEffect, transform.position + offset2, transform.rotation);
+            }
+            if (thePickup != null)
+            {
+                GameObject pickupInstance = Instantiate(thePickup, transform.position, transform.rotation);
+                Rigidbody2D rb = pickupInstance.GetComponent<Rigidbody2D>();
+                if (rb != null)
+                {
+                    rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                }
+            }
             hasInstantiated = true;
             gameObject.layer = LayerMask.NameToLayer("Default");
         }
